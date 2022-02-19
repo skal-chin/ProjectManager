bd4e3de5849d8830cafc4ac9ff9730cb33803c5c
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-02-18 19:56:50 -0700

    Service code for Task

---
+++ server/providers/services/task.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from 'server/entities/task.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class Task {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
+
+  findAllForProject(projectId: number): Promise<Task[]> {
+    return this.taskRepository.find({
+      where: { projectId },
+    });
+  }
+
+  findTaskById(id: number) {
+    return this.taskRepository.findOne(id);
+  }
+
+  createTask(task: Task): Promise<Task> {
+    return this.taskRepository.save(task);
+  }
+
+  deleteTask(task: Task) {
+    this.taskRepository.delete(task);
+  }
+}
