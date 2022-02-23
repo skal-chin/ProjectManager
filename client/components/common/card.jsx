47645e951becced08a23e75b47f20d96dc622d74
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-02-22 21:26:26 -0700

    Switched complete/incomplete

--- client/components/common/card.jsx
+++ client/components/common/card.jsx
@@ -27,9 +27,9 @@ export const Card = ({ isProject, Title, ProjectId, Description, Deadline, isAss
     <div className="bg-indigo-200 pt-3 pl-3 pr-3 m-2 shadow-md rounded-lg">
       <div className="flex justify-between mb-2">
       <span onClick={() => navigate(`${cardPath}${ProjectId}`)} className="cursor-pointer align-text-left text-2xl pb-1">{ Title }</span>
-        <button onClick={ isComplete } className={check ? "bg-purple-400 hover:bg-purple-500 text-white text-center py-1 px-3 rounded-full" :
-                                                        "bg-blue-400 hover:bg-blue-500 text-white text-center py-1 px-3 rounded-full"}>
-          { check ? "incomplete" : "complete" }</button>
+        <button onClick={ isComplete } className={check ? "bg-blue-400 hover:bg-blue-500 text-white text-center py-1 px-3 rounded-full" :
+                                                          "bg-purple-400 hover:bg-purple-500 text-white text-center py-1 px-3 rounded-full" }>
+          { check ? "complete" : "incomplete" }</button>
       </div>
 
       {isProject &&
